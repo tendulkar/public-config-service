e48a555d7af06225236f12aa3eb93d28abeb122e
Yugandhar <yugandhar@Yugandhars-MacBook-Pro.local>
2024-05-24 16:00:45 +0530

    Add datasets

---
+++ db/data/attributes.sql
@@ -0,0 +1,11 @@
+INSERT INTO attributes (namespace, family, name, label)
+VALUES ('general', 'input', 'first_name', 'Firstname'),
+    ('general', 'input', 'last_name', 'Lastname'),
+    ('general', 'input', 'email', 'Firstname'),
+    ('general', 'input', 'password', 'Password'),
+    ('general', 'input', 'phone_number', 'PHone'),
+    ('general', 'input', 'date_of_birth', 'Date of birth'),
+    ('general', 'input', 'gender', 'Gender'),
+    ('general', 'input', 'home_locaiton', 'Home location'),
+    ('general', 'input', 'first_name', 'Firstname'),
+    ('general', 'input', 'first_name', 'Firstname');
